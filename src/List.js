72e3d3264e11290dd0f1312f0801fd60999aaca5
Iris Yuan <irisjyuan@gmail.com>
2022-03-21 20:45:28 -0700

    displays the list of added items

--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ListItem from './ListItem';
 
 class List extends React.Component {
   constructor(props) {
@@ -8,17 +9,27 @@ class List extends React.Component {
     this.onClick = this.onClick.bind(this);
     this.getBall = this.getBall.bind(this);
   }
+
   onClick() {
     console.log('CLICK!!!');
     console.log(this.state.newListInput);
+    var tempArr = this.state.arr;
+    tempArr.push(this.state.newListInput);
+    this.setState({ arr: tempArr, newListInput: '' });
+    console.log(this.state.arr);
   }
+
   getBall(e) {
     this.setState({ newListInput: e.target.value });
   }
+
   render() {
     return (
       <div>
-        <input onChange={this.getBall}></input>
+        {this.state.arr.map((item, index) => (
+          <ListItem key={index} item={item}></ListItem>
+        ))}
+        <input onChange={this.getBall} value={this.state.newListInput}></input>
         <button onClick={this.onClick}>+</button>
       </div>
     );
---
+++ src/ListItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+var ListItem = (props) => {
+  return <div>{props.item}</div>;
+};
+
+export default ListItem;
