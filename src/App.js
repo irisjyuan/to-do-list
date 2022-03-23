5e575099ec2e9f7795bb76dbc691af97413f6cac
Iris Yuan <iris.yuan@loom.com>
2022-03-23 11:49:31 -0700

    validate input - no empty strings

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import List from './List';
 export function App() {
   return (
     <div className="App">
-      <h2 style={{marginTop: "5rem", marginBottom: "2rem" }}>TO DO LIST</h2>
-      <List monkey="horse" gu="po" />
+      <h2 style={{ marginTop: '5rem', marginBottom: '2rem' }}>TO DO LIST</h2>
+      <List />
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -6,19 +6,21 @@ import './List.css';
 class List extends React.Component {
   constructor(props) {
     super();
-    console.log(props);
     this.state = { arr: [], newListInput: '' };
     this.onClick = this.onClick.bind(this);
     this.getBall = this.getBall.bind(this);
   }
 
   onClick() {
-    console.log('CLICK!!!');
-    console.log(this.state.newListInput);
-    var tempArr = this.state.arr;
-    tempArr.push(this.state.newListInput);
-    this.setState({ arr: tempArr, newListInput: '' });
-    console.log(this.state.arr);
+    if (!this.state.newListInput) {
+      alert(
+        'you have to enter something in the input box to add it to the to do list'
+      );
+    } else {
+      var tempArr = this.state.arr;
+      tempArr.push(this.state.newListInput);
+      this.setState({ arr: tempArr, newListInput: '' });
+    }
   }
 
   getBall(e) {
@@ -31,13 +33,15 @@ class List extends React.Component {
         {this.state.arr.map((item, index) => (
           <ListItem key={index} item={item}></ListItem>
         ))}
-        <div className='input-container'>
+        <div className="input-container">
           <input
             onChange={this.getBall}
             value={this.state.newListInput}
             placeholder="Add a todo"
           ></input>
-          <button className='input-button' onClick={this.onClick}>+</button>
+          <button className="input-button" onClick={this.onClick}>
+            +
+          </button>
         </div>
       </div>
     );
